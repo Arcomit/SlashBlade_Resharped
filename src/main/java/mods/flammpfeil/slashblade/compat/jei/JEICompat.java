5c62c1c06cbc69d2d3091a75bb78d1a431459943
Arcomit <8089341@qq.com>
2025-06-14 14:05:03 +0800

    1.修复了JEI作弊模式拿拔刀剑会变成无铭的BUG #96 #94

--- src/main/java/mods/flammpfeil/slashblade/compat/jei/JEICompat.java
+++ src/main/java/mods/flammpfeil/slashblade/compat/jei/JEICompat.java
@@ -2,10 +2,14 @@ package mods.flammpfeil.slashblade.compat.jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.UidContext;
 import mezz.jei.api.registration.ISubtypeRegistration;
 import mods.flammpfeil.slashblade.SlashBlade;
 import mods.flammpfeil.slashblade.init.SBItems;
+import mods.flammpfeil.slashblade.item.ItemSlashBlade;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 @JeiPlugin
 public class JEICompat implements IModPlugin {
@@ -18,7 +22,16 @@ public class JEICompat implements IModPlugin {
 	@Override
 	public void registerItemSubtypes(ISubtypeRegistration registration) {
 		registration.registerSubtypeInterpreter(SBItems.slashblade,
-				(stack, context) -> stack.getOrCreateTagElement("bladeState").getString("translationKey"));
+				(stack, context) -> {
+					//同步nbt到Cap
+					stack.getCapability(ItemSlashBlade.BLADESTATE).ifPresent(cap -> {
+								cap.deserializeNBT(stack.getOrCreateTag().getCompound("bladeState"));
+							}
+					);
+					return stack.getCapability(ItemSlashBlade.BLADESTATE)
+							.map(cap -> cap.getTranslationKey())
+							.orElse("");
+				});
 	}
 
 }
