507eaa28d2d00fcd22e28ae4c96537a8c694988c
Tonywww <1204484415@qq.com>
2025-09-08 02:25:53 +0800

    锻造台配方EMI兼容

--- src/main/java/mods/flammpfeil/slashblade/recipe/RecipeSerializerRegistry.java
+++ src/main/java/mods/flammpfeil/slashblade/recipe/RecipeSerializerRegistry.java
@@ -1,12 +1,17 @@
 package mods.flammpfeil.slashblade.recipe;
 
 import mods.flammpfeil.slashblade.SlashBlade;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class RecipeSerializerRegistry {
+    public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister
+            .create(ForgeRegistries.RECIPE_TYPES, SlashBlade.MODID);
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZER = DeferredRegister
             .create(ForgeRegistries.RECIPE_SERIALIZERS, SlashBlade.MODID);
 
@@ -18,4 +23,15 @@ public class RecipeSerializerRegistry {
     
     public static final RegistryObject<RecipeSerializer<?>> SLASHBLADE_SMITHING = RECIPE_SERIALIZER
             .register("slashblade_smithing", () -> SlashBladeSmithingRecipe.SERIALIZER);
+
+    public static final RegistryObject<RecipeType<SlashBladeSmithingRecipe>> SLASHBLADE_SMITHING_TYPE = RECIPE_TYPES
+            .register("slashblade_smithing", () -> recipeType("slashblade_smithing"));
+
+    private static <T extends Recipe<?>> RecipeType<T> recipeType(String name) {
+        return new RecipeType<T>() {
+            public String toString() {
+                return new ResourceLocation(SlashBlade.MODID, name).toString();
+            }
+        };
+    }
 }
