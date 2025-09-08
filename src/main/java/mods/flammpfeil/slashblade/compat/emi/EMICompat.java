2f897ec1764c5773ff8c09df304d78ef77c35f40
Tonywww <1204484415@qq.com>
2025-09-08 23:36:35 +0800

    EMI Capability兼容 P2

--- src/main/java/mods/flammpfeil/slashblade/compat/emi/EMICompat.java
+++ src/main/java/mods/flammpfeil/slashblade/compat/emi/EMICompat.java
@@ -11,6 +11,7 @@ import mods.flammpfeil.slashblade.recipe.RecipeSerializerRegistry;
 import mods.flammpfeil.slashblade.recipe.SlashBladeShapedRecipe;
 import mods.flammpfeil.slashblade.recipe.SlashBladeSmithingRecipe;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -56,6 +57,8 @@ public class EMICompat implements EmiPlugin {
                 .forEach(stack -> {
                     registry.addEmiStack(EMISlashBladeStack.of(stack));
                 });
+
+        registry.removeRecipes(new ResourceLocation("emi", "/crafting/repairing/slashblade/slashblade"));
     }
 
     private static <C extends Container, T extends Recipe<C>> List<T> findRecipesByType(RecipeType<T> type) {
