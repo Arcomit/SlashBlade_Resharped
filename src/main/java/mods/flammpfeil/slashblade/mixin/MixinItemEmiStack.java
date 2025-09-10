84481ebbc86ad9e8f0d6b46695653c9f6212981a
Tonywww <1204484415@qq.com>
2025-09-10 11:54:08 +0800

    EMi配方匹配和EMI神秘的U

--- src/main/java/mods/flammpfeil/slashblade/mixin/MixinItemEmiStack.java
+++ src/main/java/mods/flammpfeil/slashblade/mixin/MixinItemEmiStack.java
@@ -1,6 +1,5 @@
 package mods.flammpfeil.slashblade.mixin;
 
-import dev.emi.emi.api.stack.Comparison;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.stack.ItemEmiStack;
 import mods.flammpfeil.slashblade.item.ItemSlashBlade;
@@ -11,19 +10,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static mods.flammpfeil.slashblade.compat.emi.EMIUtils.SLASHBLADE_COMPARISON;
+
 @Mixin(value = ItemEmiStack.class, remap = false)
 public abstract class MixinItemEmiStack extends EmiStack {
-
-    private static Comparison SLASHBLADE_COMPARSION = Comparison.of((self, other) -> {
-        ItemStack aStack = self.getItemStack();
-        ItemStack bStack = other.getItemStack();
-        if (aStack.getItem() != bStack.getItem()) return false;
-        String keyA = self.getNbt().getCompound("bladeState").getString("translationKey");
-        String keyB = other.getNbt().getCompound("bladeState").getString("translationKey");
-
-        return keyB.equals(keyA);
-    });
-
     @Unique
     public ItemStack slashBlade_Resharped$origionalStack;
 
@@ -31,7 +21,7 @@ public abstract class MixinItemEmiStack extends EmiStack {
     private void onConstructor(ItemStack stack, CallbackInfo ci) {
         if (stack.getItem() instanceof ItemSlashBlade) {
             this.slashBlade_Resharped$origionalStack = stack.copy();
-            this.comparison = SLASHBLADE_COMPARSION;
+            this.comparison = SLASHBLADE_COMPARISON;
         }
     }
 
@@ -49,7 +39,7 @@ public abstract class MixinItemEmiStack extends EmiStack {
             EmiStack stack = new ItemEmiStack(this.slashBlade_Resharped$origionalStack);
             stack.setChance(origin.getChance())
                     .setRemainder(origin.getRemainder().copy())
-                    .comparison(SLASHBLADE_COMPARSION);
+                    .comparison(SLASHBLADE_COMPARISON);
 
             cir.setReturnValue(stack);
         }
