ef6ce9467775ee1e3e593fdc95648eae539411ca
Tonywww <1204484415@qq.com>
2025-09-28 16:37:47 +0800

    修改BladeMotionEvent

--- src/main/java/mods/flammpfeil/slashblade/event/BladeMotionEvent.java
+++ src/main/java/mods/flammpfeil/slashblade/event/BladeMotionEvent.java
@@ -2,8 +2,10 @@ package mods.flammpfeil.slashblade.event;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
+@Cancelable
 public class BladeMotionEvent extends Event {
     private final LivingEntity entity;
     private final ResourceLocation combo;
